50d941ee820d291cd03c702e279cd58f5b49c087
Kartikeya <kartvat3@in.ibm.com>
2017-03-13 10:57:39 +0530

    files

---
+++ greetings.js
@@ -0,0 +1,14 @@
+// greetings.js
+var request = require('request');
+var exports = module.exports = {};
+exports.callRAPI = function(query,callback){
+request('https://hello-world-flask-premed-interpolation.mybluemix.net'+ query, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        callback(null,body); //print the list
+    }
+    else{
+        callback("ERROR", null);
+    }
+})
+};
+
